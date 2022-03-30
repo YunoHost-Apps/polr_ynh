da5153ec7eb33f5e1070a30f226df44201827b8f
ericgaspar <junk.eg@free.fr>
2022-03-31 00:48:36 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ YNH_PHP_VERSION=7.3
 # Composer version
 YNH_COMPOSER_VERSION="2.2.9"
 
-pkg_dependencies="php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-exif php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysqli"
+pkg_dependencies="php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-exif php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql"
 
 #=================================================
 # PERSONAL HELPERS
