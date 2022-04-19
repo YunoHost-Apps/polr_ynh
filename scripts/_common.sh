e62bd2bf8e4d69bfc7e12ede6bdad072772bfaab
ericgaspar <junk.eg@free.fr>
2022-04-19 15:06:57 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 YNH_PHP_VERSION=7.3
 
 # Composer version
-YNH_COMPOSER_VERSION="2.2.9"
+YNH_COMPOSER_VERSION="2.3.3"
 
 pkg_dependencies="php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql"
 
