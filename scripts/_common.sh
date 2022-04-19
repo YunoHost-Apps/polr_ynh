6873b0ee268e3cb57bb1877b98ddd1ef9cf16008
ericgaspar <junk.eg@free.fr>
2022-04-19 16:54:59 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ YNH_PHP_VERSION=7.3
 # Composer version
 YNH_COMPOSER_VERSION="2.3.3"
 
-pkg_dependencies="php${YNH_PHP_VERSION}-memcache php${YNH_PHP_VERSION}-memcached php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql"
+pkg_dependencies="memcached libmemcached-tools php${YNH_PHP_VERSION}-memcache php${YNH_PHP_VERSION}-memcached php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql"
 
 #=================================================
 # PERSONAL HELPERS
